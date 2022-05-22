2d8c71a6c8468f3f9d2e1fb283175f366e19d215
Narek <narekazibekyan5@gmail.com>
2022-05-22 20:29:16 +0400

    Cube numbers

---
+++ Task_23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на
+// вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 4, 9.
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int Y = 1;
+int sum = 0;
+while (Y <= N)
+{
+    sum = Y*Y*Y;
+    Console.WriteLine($"{Y}*{Y}*{Y} = {sum}");
+    Y++;
+}
